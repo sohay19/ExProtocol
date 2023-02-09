981cc75b83b5d0fd37753f422bd632c0b8af618c
soha <sa4022@naver.com>
2023-02-09 16:32:46 +0900

    Initial Commit

---
+++ ExProtocol/ExProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExProtocolApp.swift
+//  ExProtocol
+//
+//  Created by 소하 on 2023/02/09.
+//
+
+import SwiftUI
+
+@main
+struct ExProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExProtocolUITests/ExProtocolUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ExProtocolUITestsLaunchTests.swift
+//  ExProtocolUITests
+//
+//  Created by 소하 on 2023/02/09.
+//
+
+import XCTest
+
+final class ExProtocolUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
